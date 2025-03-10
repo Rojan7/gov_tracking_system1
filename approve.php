f24fef672cd31087d1669577fd39bd3d9f5d8ecd
Rojan7 <rojanadhikari02@gmail.com>
2025-03-10 13:54:46 +0545

    removed unwanted systems

--- approve.php
+++
@@ -1,6 +0,0 @@
-<?php include 'config.php'; session_start();
-if (!isset($_SESSION['admin_id'])) header('Location: admin_login.php');
-$id = $_GET['id'];
-$conn->query("UPDATE applications SET status='Approved' WHERE id=$id");
-header('Location: admin_dashboard.php');
-?>
